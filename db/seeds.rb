208b2c6d350f9d06d8bafbe28c7094e290870587
LEARN Academy <>
2021-04-01 10:39:18 -0700

    added api seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cats = [
+  {
+    name: 'Bob',
+    age: 2,
+    enjoys: 'Likes to eat shoes.'
+  },
+  {
+    name: 'Myla',
+    age: 12,
+    enjoys: 'Food mostly, really just food, lots of food.'
+  },
+  {
+    name: 'Dozer',
+    age: 5,
+    enjoys: 'Likes bumping into things.'
+  }
+]
+
+cats.each do |cat|
+  Cat.create cat
+  puts "creating cat: #{cat}"
+end
